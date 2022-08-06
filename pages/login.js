f140cb9ea9d8b22d58a1256a3a2b410e82cae3d5
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-06 08:28:28 +0530

    login page

--- pages/login.js
+++ pages/login.js
@@ -1,5 +1,20 @@
+import React from 'react';
 function Login() {
-  return <div>Login page </div>;
+  const usernameRef = React.useRef();
+  const passwordRef = React.useRef();
+
+  const hs = () => {};
+
+  return (
+    <div>
+      <h1>Login page</h1>
+
+      <input value="mor_2314" ref={usernameRef} placeholder="username" />
+      <input value="83r5^_" ref={passwordRef} placeholder="username" />
+
+      <button onClick={hs}>login me </button>
+    </div>
+  );
 }
 
 export default Login;
