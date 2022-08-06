b101c8d8e6a0174b43d8ad603772ed2fd1ce3b78
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-06 07:54:55 +0530

    header added

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,29 @@
 import React from 'react';
+import Link from 'next/link';
+
 function Header() {
-  return <div>header</div>;
+  return (
+    <div>
+      <div>Ecommerce</div>
+      <div>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+        <Link href="/login">
+          <a>Login</a>
+        </Link>
+        <Link href="/signup">
+          <a>signup</a>
+        </Link>
+        <Link href="/cart">
+          <a>cart</a>
+        </Link>
+        <Link href="/orders">
+          <a>orders</a>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
