c5e858b02d1f744d956ff3c519c9dcf135e6cfb4
nasagaurav <nasa.gauravnasa@gmail.com>
2022-08-07 08:09:50 +0530

    added categories css with TW

--- pages/index.js
+++ pages/index.js
@@ -2,18 +2,27 @@ import Link from 'next/link';
 import Card from '../components/Card';
 function Home({ products, categories }) {
   return (
-    <div>
-      <h1>Home page</h1>
-      <div>
-        <h3>all categories {categories.length}</h3>
+    <div className="main p-5">
+      <div className="all-cat">
+        <h3 className="text-lg mb-5">all categories {categories.length}</h3>
         {categories.map((x) => (
           <Link key={x} href={`/category/${x}`}>
-            <a>{x}</a>
+            <a
+              className="
+            m-2 
+            bg-[tomato] text-white p-2 
+            rounded-full  
+            hover:shadow-[0px_2px_10px_grey] 
+            hover:bg-[slateblue] 
+            
+            "
+            >
+              {x}
+            </a>
           </Link>
         ))}
       </div>
-      <div>
-        <h3>all products {products.length}</h3>
+      <div className="products mt-5">
         {products.map((item, index) => (
           <Card key={index} {...item} />
         ))}
